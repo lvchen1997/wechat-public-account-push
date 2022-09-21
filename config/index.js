1a1d7e1eb26f66983c64d43aac4bc6ef4df5f925
lvchen1997 <58586268+lvchen1997@users.noreply.github.com>
2022-09-21 11:36:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "f4911f5e5e97176cddec0c68f714e9f8",
+  APP_SECRET: "eb1f5bf98866fdde29fb219bc36f0099",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
